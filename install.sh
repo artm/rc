515b9e79fac156551d953d76afe20bfbb49f2d2e
Artem Baguinski <abaguinski@depraktijkindex.nl>
2013-01-28 14:12:20 +0100

    more install script's fixes

--- install.sh
+++ install.sh
@@ -4,28 +4,30 @@ RC=$(basename $(pwd))
 SELF=$(basename $0)
 
 for file in * ; do
-  case $file in
+  case "$file" in
     "$BACKUP_DIR"|"Readme.*"|"$SELF")
       continue
       ;;
     *)
       link="../.$file"
-      if [ -L "$link" -a $(readlink $link) == "rc/$file" ] ; then
+      link_to="$RC/$file"
+      if [ "$(readlink $link)" == "$link_to" ] ; then
         # silently skip existing
         echo "Keeping $file"
         continue
       elif [ -e $link ] ; then
-        if [ ! -e $BACKUP_DIR/$file ] ; then
+        backup="$BACKUP_DIR/$file"
+        if [ ! -e "$backup" ] ; then
           echo "BACKUP .$file"
           mkdir -p "$BACKUP_DIR"
-          mv "$link" "$BACKUP_DIR/$file"
+          mv "$link" "$backup"
         else
           echo "WARNING: wanted to backup $link but $BACKUP_DIR/$file already exists"
           continue
         fi
       fi
       echo "Installing $file symlink"
-      ln -s "rc/$file" "$link"
+      ln -s "$link_to" "$link"
       ;;
   esac
 done
