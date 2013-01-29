dbf12f46c4f1a00931365db331079797b5e5b373
Artem Baguinski <abaguinski@depraktijkindex.nl>
2013-01-29 22:17:37 +0100

    -u to uninstall

--- install.sh
+++ install.sh
@@ -3,6 +3,58 @@ BACKUP_DIR=bak
 RC=$(basename $(pwd))
 SELF=$(basename $0)
 
+operation=install
+
+while getopts "?hu" opt; do
+  case $opt in
+    ?|h)
+      echo Usage: $(basename $0) [-h] [-u]
+      echo
+      echo "-h    help"
+      echo "-u    uninstall instead of installing"
+      exit
+      ;;
+    u)
+      operation=uninstall
+      ;;
+  esac
+done
+
+function install()
+{
+  file=$1
+  link=$2
+  link_to=$3
+  backup=$4
+  if [ "$(readlink $link)" == "$link_to" ] ; then
+    echo "Keeping $file"
+    continue
+  elif [ -e "$link" ] ; then
+    if [ ! -e "$backup" ] ; then
+      echo "BACKUP .$file"
+      mkdir -p "$BACKUP_DIR"
+      mv "$link" "$backup"
+    else
+      echo "WARNING: wanted to backup $link but $BACKUP_DIR/$file already exists"
+      continue
+    fi
+  fi
+  echo "Installing $file symlink"
+  ln -s "$link_to" "$link"
+}
+
+function uninstall()
+{
+  file=$1
+  link=$2
+  link_to=$3
+  backup=$4
+  if [ "$(readlink $link)" == "$link_to" ] ; then
+    echo "Removing $file symlink"
+    rm "$link"
+  fi
+}
+
 for file in * ; do
   case "$file" in
     "$BACKUP_DIR"|"Readme.*"|"$SELF")
@@ -11,23 +63,8 @@ for file in * ; do
     *)
       link="../.$file"
       link_to="$RC/$file"
-      if [ "$(readlink $link)" == "$link_to" ] ; then
-        # silently skip existing
-        echo "Keeping $file"
-        continue
-      elif [ -e $link ] ; then
-        backup="$BACKUP_DIR/$file"
-        if [ ! -e "$backup" ] ; then
-          echo "BACKUP .$file"
-          mkdir -p "$BACKUP_DIR"
-          mv "$link" "$backup"
-        else
-          echo "WARNING: wanted to backup $link but $BACKUP_DIR/$file already exists"
-          continue
-        fi
-      fi
-      echo "Installing $file symlink"
-      ln -s "$link_to" "$link"
+      backup="$BACKUP_DIR/$file"
+      $operation $file $link $link_to $backup
       ;;
   esac
 done
