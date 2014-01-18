463b3ebfcac5f4e807fd2ac6a8ba93d2e0b221f1
Artem Baguinski <abaguinski@depraktijkindex.nl>
2014-01-18 15:18:19 +0100

    import key; exercism; editorconfig

---
+++ bin/import_key.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+if [ -z "$1" ] ; then
+  echo "Usage: $0 KEY-HASH ..."
+  exit 1
+fi
+
+for key_hash in $@ do
+  gpg --recv-keys $key_hash && gpg --export --armor $key_hash | sudo apt-key add -
+done
